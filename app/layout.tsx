aa564ff2d8992a8c790eb6291100c1d17740f63d
nayaracalenzo <queiroz.14@hotmail.com>
2025-05-30 19:21:35 -0300

    adicionando SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,30 @@ import GoogleAnalytics from './components/GoogleAnalytics';
 import { poppins } from './fonts';
 
 export const metadata = {
-    title: 'Lis Tur',
-    description: 'Descubra as praias mais incríveis do Ceará com a Listur, sua agência de turismo especializada em roteiros inesquecíveis pelo litoral nordestino.',
+    title: 'Listur | Agência de Turismo',
+    description:
+      'Descubra as praias mais incríveis do Ceará com a Listur, sua agência de turismo especializada em roteiros inesquecíveis pelo litoral nordestino.',
+    keywords: ['turismo', 'praias do Ceará', 'Listur', 'viagem', 'roteiros', 'agência de turismo', 'fortaleza', 'passeio'],
+    openGraph: {
+      title: 'Listur | Agência de Turismo',
+      description:
+        'Descubra as praias mais incríveis do Ceará com a Listur, sua agência de turismo especializada em roteiros inesquecíveis pelo litoral nordestino.',
+      url: 'https://www.listur.com', // substitua pela URL real
+      siteName: 'Listur',
+      images: [
+        {
+          url: '/og-image.jpg',
+          width: 1200,
+          height: 630,
+          alt: 'Praia paradisíaca do Ceará',
+        },
+      ],
+      locale: 'pt_BR',
+      type: 'website',
+    },
+    icons: {
+      icon: '/listurlogo1.png',
+    },
   };
 
 export default function RootLayout({
