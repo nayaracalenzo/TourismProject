66cd19e60b6b5d4fc9093b40fc48b2e4e09edf54
nayaracalenzo <queiroz.14@hotmail.com>
2025-05-30 19:05:00 -0300

    adicionando google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 'use client';
 import './globals.css';
+import GoogleAnalytics from './components/GoogleAnalytics';
 import { poppins } from './fonts';
+
+export const metadata = {
+    title: 'Lis Tur',
+    description: 'Descubra as praias mais incríveis do Ceará com a Listur, sua agência de turismo especializada em roteiros inesquecíveis pelo litoral nordestino.',
+  };
 
 export default function RootLayout({
   children,
@@ -9,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={poppins.className}>{children}</body>
+        <body className={poppins.className}>
+            <GoogleAnalytics gaId="G-PBLY4FYN3P" />
+            {children}
+        </body>
     </html>
   );
 }
